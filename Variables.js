5d535ea4d83c189e68e8ba2accbe3c7e9c82e10e
Ravi Raj <tiwariraviraj50@gmail.com>
2025-12-16 20:32:38 +0530

    1 commit

---
+++ Variables.js
@@ -0,0 +1,21 @@
+// What is variable ?
+// ans: - a variable is a container its. store the data.
+// there are two types of varible is use in javascript Es6 version use :
+// 1. let :-  in the block and change the value of variable 
+// example: - 
+
+let age = 22;
+ age = 23;
+ console.log(age); 
+  
+let name = "Raj";
+ name = "Ravi";
+ console.log(name); 
+
+// 2. const : - in the block but value can not be change and impotant is thing when without value
+//  not declare any varible.
+// example: -
+const user =  "Ravi";
+//  const user = "Raj";  not allowed 
+user.name ="raj";  // when we pass the object reference then we will change the value. 
+ console.log(user);    
