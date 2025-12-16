6a72372d2c2cc8c46cdb127f7192f91e549bb48c
Ravi Raj <tiwariraviraj50@gmail.com>
2025-12-16 21:13:45 +0530

    3 COMMIT

---
+++ array.js
@@ -0,0 +1,7 @@
+// Array kya hota hai?
+
+// Array multiple values ko ek variable me store karta hai.
+// example: -
+
+let skills = ['Html', 'Css', 'Javascript' ];
+console.log(skills);
---
+++ object.js
@@ -0,0 +1,17 @@
+// Object kya hota hai?
+
+// Object key–value pair me data store karta hai.
+// Ye real-world entity ko represent karta hai.
+
+//  Example: ek user ke paas name, age, skills hoti hain.
+
+const user ={
+    name:"Ravi",
+    age:23,
+    role:"Developer" 
+}
+user.name = "Raj";
+console.log(user); // name,age,role tino print honge
+console.log(user.name); // Ravi
+console.log(user.age); // Ravi
+console.log(user.role); // Ravi
